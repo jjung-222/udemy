b2c2cd5637287907fadb8f00e9d0fed8304b6e3f
jjung-222 <lyj7877@naver.com>
2023-07-24 23:41:06 +0900

    jsx practice

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-function App() {
+import ExpenseItem from "./components/ExpenseItem.js"; //이렇게 임포트 하면 이 파일을 이제 html 요소처럼 사용 가능
+
+function App() { 
+  //App.js는 루트 컴포넌트로서 모든 컴포넌트들은 이 안에서 중첩되어서 사용된다.
+  //리액트는 컴포넌트 트리로 구성되어져 있다. 맨 위에 App.js 컴포넌트 존재
+
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem />
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,19 @@
+import './ExpenseItem.css'
+
+function ExpenseItem () {
+
+    //중요한 규칙이 하나 있습니다. 반환하는 문장마다 또는 JSX코드 조각마다 반드시 한 개의 루트 요소를 갖는다
+    return  (
+    <div className="expense-item">
+        <div>March 28th 2021</div>
+        <div className="expense-item__description">
+            <h2>Car Insurance</h2>
+            <div className="expense-item__price">$294.50</div>
+        </div>
+    </div>
+    );
+}
+
+//컴포넌트를 사용하기 위해 내보내야함. 그렇지 않으면 그 파일 안에서만 사용가능
+// export default 란? 이 함수를 이 파일의 기본 함수로 내보내겠다.
+export default ExpenseItem;
